3b455db1250af4c0edac72630c41c039c2aa2669
Your Name <you@example.com>
2024-08-29 12:55:36 +0700

    pws

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cubau79oy$!kr$0+hs&f#-ufz#kh1_7-*&3vv#u_^!1$fgd@y3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "sean-farrel-mentalhealthtracker2.pbp.cs.ui.ac.id"]
 
 
 # Application definition
