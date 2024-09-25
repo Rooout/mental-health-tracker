d8fd7096b203ec5cda48c0a529cdc9a54ecb68a9
Your Name <you@example.com>
2024-09-25 15:52:57 +0700

    tutorial 4

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -134,5 +134,4 @@ else:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/web/project/sean.farrel/mentalhealthtracker", "https://pbp.cs.ui.ac.id/web/project/sean.farrel/mentalhealthtracker"]
-
 # Nambahin akses URL
